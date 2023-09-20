06eacdafc7bac506e1553ddd34d2fbd88c968e49
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-09-20 21:48:47 +0530

    stable db connection

--- prisma/index.ts
+++ prisma/index.ts
@@ -45,14 +45,14 @@ async function main() {
 	// console.log(test1);
 }
 
-main()
-	.then(async () => {
-		await prisma.$disconnect();
-	})
-	.catch(async (e) => {
-		console.error(e);
-		await prisma.$disconnect();
-		process.exit(1);
-	});
+// main()
+// 	.then(async () => {
+// 		await prisma.$disconnect();
+// 	})
+// 	.catch(async (e) => {
+// 		console.error(e);
+// 		await prisma.$disconnect();
+// 		process.exit(1);
+// 	});
 
 export default prisma;
