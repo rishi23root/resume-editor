6e99c320cfbe58341500428e2258459b40df4f85
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-09-20 21:20:41 +0530

    base schema finalized

--- prisma/index.ts
+++ prisma/index.ts
@@ -15,44 +15,44 @@ const prisma = new PrismaClient({
 	errorFormat: "pretty",
 });
 
-// async function main() {
-// 	// Connect the client
-// 	await prisma.$connect();
-// 	// ... you will write your Prisma Client queries here
-// 	// prisma.$connect();
-// 	// delete all users with urlTable
-// 	// const test = await prisma.user.deleteMany({
-// 	// 	where: {
-// 	// 		UrlTable: {
-// 	// 			some: {},
-// 	// 		},
-// 	// 	},
-// 	// });
-// 	// console.log(test);
-// 	// const test1 = await prisma.user.create({
-// 	// 	data: {
-// 	// 		name: "admin",
-// 	// 		email: "admin@admin.com",
-// 	// 		password: "admin",
-// 	// 		UrlTable: {
-// 	// 			create: {
-// 	// 				url: "https://www.google.com",
-// 	// 				slug: "gogle",
-// 	// 			},
-// 	// 		},
-// 	// 	},
-// 	// });
-// 	// console.log(test1);
-// }
+async function main() {
+	// Connect the client
+	await prisma.$connect();
+	// ... you will write your Prisma Client queries here
+	// prisma.$connect();
+	// delete all users with urlTable
+	// const test = await prisma.user.deleteMany({
+	// 	where: {
+	// 		UrlTable: {
+	// 			some: {},
+	// 		},
+	// 	},
+	// });
+	// console.log(test);
+	// const test1 = await prisma.user.create({
+	// 	data: {
+	// 		name: "admin",
+	// 		email: "admin@admin.com",
+	// 		password: "admin",
+	// 		UrlTable: {
+	// 			create: {
+	// 				url: "https://www.google.com",
+	// 				slug: "gogle",
+	// 			},
+	// 		},
+	// 	},
+	// });
+	// console.log(test1);
+}
 
-// main()
-// 	.then(async () => {
-// 		await prisma.$disconnect();
-// 	})
-// 	.catch(async (e) => {
-// 		console.error(e);
-// 		await prisma.$disconnect();
-// 		process.exit(1);
-// 	});
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
 
 export default prisma;
