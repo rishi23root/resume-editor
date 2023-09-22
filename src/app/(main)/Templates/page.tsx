617f08b0ea7602941962ad81e649c435f43604a8
Rishabh Jain <66913499+rishi23root@users.noreply.github.com>
2023-09-22 16:35:47 +0000

    template page started

--- src/app/(main)/Templates/page.tsx
+++ src/app/(main)/Templates/page.tsx
@@ -1,21 +1,50 @@
-"use client";
+// "use client";
+async function getData() {
+  const res = await fetch(`${process.env.BACKEND}/templates`)
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-import TwScreenInfo from "../../../components/custom/TwScreenInfo";
-import AnimateText from "../../../components/custom/AmimateText";
-import { AnimatePresence, motion } from "framer-motion";
-import { Skeleton } from "@/components/ui/skeleton";
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
 
-// import { UserButton, useAuth, useUser } from "@clerk/nextjs";
+  return res.json()
+}
 
-export default function Template() {
+
+export default async function Template() {
   // get userid from clerk
   // const { isLoaded, userId, sessionId, getToken } = useAuth();
   // const { user } = useUser();
   // console.log(userId, user);
 
+  const data: string[] = await getData()
+  console.log(data)
+
   // check if user is signed in if not then redirect to login pages else redirect to dashboard
   return (
     // make whole page with 10/12 width and center it on above medium screens
-    <div className="border"> this is Templates </div>
+    <div className="w-full">
+      <div className="showcase border w-1/6 fc gap-4 p-3 h-full items-center">
+        <h1 className="text-2xl">Templates</h1>
+        {
+          data.map(ele => {
+            return (
+              <div className="templateCointainer w-[70%] border h-1/4">
+
+                <div className="template w-[70%] border h-1/4 rounded-sm">
+                  {/* 
+                  // image of template
+                */}
+                  image
+                </div>
+                {ele}
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   );
 }
