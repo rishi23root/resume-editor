7134017b566ea9dc61570ee5659367abd75b8dd6
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-10-22 04:26:20 +0530

    Fix #30

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,34 +1,7 @@
-const BubbleUnderlay = dynamic(
-  () => import("@/components/custom/BubbelUnderLay")
-);
-
-import { cn } from "@/lib/utils";
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-import NoSSR from "../../hooks/NoSSR";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="m-auto fcc">
-      <Suspense>
-        <NoSSR>
-          <BubbleUnderlay
-            className={cn(
-              "fixed",
-              "top-0 left-0",
-              "w-full h-full",
-              "-z-10",
-              "pointer-events-none",
-              "select-none"
-            )}
-          />
-        </NoSSR>
-      </Suspense>
-      {children}
-    </div>
-  );
+  return <div className="m-auto fcc">{children}</div>;
 }
