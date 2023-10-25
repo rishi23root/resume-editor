1f0f2f8dcae8a5454219cb07227d575ce5156c42
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-10-25 12:16:11 +0530

    added some global components

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,8 @@
 import Nav from "@/components/Nav/index";
 import { userLogined } from "@/utils/serverActions/pageLoad";
 
+import { Toaster } from "@/components/ui/toaster";
+
 export default async function RootLayout({
   children,
 }: {
@@ -11,6 +13,8 @@ export default async function RootLayout({
 
   return (
     <>
+      {/* addeding toast component */}
+      <Toaster />
       <div className="app xl:px-[11%] md:px-[5%] px-[2%] py-[2.5rem] flex flex-col lg:gap-20 gap-8 w-full">
         <Nav isSignedIn={data ? true : false} pathname="/dashboard" />
         {/* pathname value is hardcoded here because it doesn't matter which path it is just not '/' */}
