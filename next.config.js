47a0e4293257ffac54d30eefcb3073b5adbc1370
Rishabh Jain <rishabhjainfinal@gmail.com>
2024-01-29 02:03:03 +0530

    close #4, seems of no need for now

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
-    // headers() {
     // https://report-uri.com/home/generate to solve it 
+    // headers() {
     //     return [
     //         {
     //             source: '/(.*)',
@@ -21,8 +21,10 @@ const nextConfig = {
     //                 },
     //                 {
     //                     key: 'Content-Security-Policy',
-    //                     value:
-    //                         `"default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline';`,
+    //                     value: "default-src 'self'; script-src 'report-sample' 'self' ; style-src 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self'; font-src 'self'; frame-src 'self'; img-src 'self'; manifest-src 'self'; media-src 'self'; report-uri https://65b6adae73eb9d58ab9f74e1.endpoint.csper.io/?v=0; worker-src 'none';"
+    //                         // `Content-Security-Policy: default-src 'self' data:; script-src 'self' data:; style-src 'self'; img-src *; media-src *; object-src *`,
+    //                         // "Content-Security-Policy: default-src 'self' data:; script-src * 'self' data: 'unsafe-inline' 'unsafe-hashes' 'unsafe-eval'; style-src 'self'; img-src *; media-src *; object-src *"
+    //                         // `"default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline';`,
     //                 }
     //             ],
     //         },
