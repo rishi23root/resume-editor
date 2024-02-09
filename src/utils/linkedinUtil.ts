b3e5b6b751f8903f1c95d307b730f75ebee85d1b
Rishabh Jain <rishabhjainfinal@gmail.com>
2024-02-09 16:44:16 +0530

    updated #55

--- src/utils/linkedinUtil.ts
+++ src/utils/linkedinUtil.ts
@@ -3,16 +3,16 @@
 import { ExternalAccount, User, currentUser } from "@clerk/nextjs/server";
 
 
-export async function checkIfFromLinkedin(user:User|null): Promise< ExternalAccount| Boolean> {
+export async function checkIfFromLinkedin(user: User | null): Promise<ExternalAccount | Boolean> {
     // loop through the accounts and check provider
-    var data = user?.externalAccounts.filter(account => account.verification?.strategy.includes('oauth_linkedin')) as ExternalAccount[];
-    var account : ExternalAccount| Boolean = false;
+    var data = user?.externalAccounts?.filter(account => account.verification?.strategy?.includes('oauth_linkedin')) as ExternalAccount[];
+    var account: ExternalAccount | Boolean = false;
     if (data.length > 0) account = data[0]
     return account;
 }
 
 // get linkedin auto from cleck backend 
-export async function getLinkedinAuth(): Promise<null|any> {
+export async function getLinkedinAuth(): Promise<null | any> {
     const user = await currentUser();
     return {
         error: "unable to integrate due to linkedin authentication constraints"
@@ -23,12 +23,12 @@ export async function getLinkedinAuth(): Promise<null|any> {
         {
             method: "GET",
             headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.CLERK_SECRET_KEY as string}`,
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.CLERK_SECRET_KEY as string}`,
             },
         }
     );
-    if (res.status == 200){
+    if (res.status == 200) {
         const data = await res.json();
         // extract the account info from backend, using fetch request from the server   
         // console.log(data);   
@@ -48,7 +48,7 @@ export async function getLinkedinAuth(): Promise<null|any> {
 
 
 // extract linkedin data using auth form linkedin auth api
-async function extractLinkedinResumeData(token:any){
+async function extractLinkedinResumeData(token: any) {
     console.log(token);
     // let testUrl = "https://api.linkedin.com/v2/me/~:(id,email-address,first-name,last-name),educations:(id,school-name,field-of-study,start-date,end-date,degree,activities,notes),positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,size,industry,ticker)))?format=json"
     // const res = await fetch(testUrl,{
@@ -60,7 +60,7 @@ async function extractLinkedinResumeData(token:any){
     // })
     // console.log(await res.json());
 
-    
+
     // need to work on elivating the scope permissions for the auth
     throw new Error("Not implemented because of api limitations")
 
