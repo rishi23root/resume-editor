44773d18bd2419c21176582fa9368787976082ee
Rishabh Jain <rishabhjainfinal@gmail.com>
2024-02-21 10:48:54 +0530

    fix robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,10 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      // disallow: '/*',
     },
-    sitemap: `/sitemap.xml`,
   }
-}
+} 
