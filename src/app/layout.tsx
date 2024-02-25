5d423aa29b5d21659e5e4a7c9079bd1b3d286ebe
Rishabh Jain <rishabhjainfinal@gmail.com>
2024-02-25 11:38:11 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,28 +4,28 @@ import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { clsx } from "clsx";
 import type { Metadata } from "next";
 import dynamic from "next/dynamic";
 import { Poppins } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import { Suspense } from "react";
-import type { Viewport } from 'next'
+import type { Viewport } from "next";
 const BubbleUnderlay = dynamic(
   () => import("@/components/custom/BubbelUnderLay")
 );
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
- 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
-}
+};
 
 export const metadata: Metadata = {
   title: "Build.Your.Resume online",
@@ -111,6 +111,7 @@ export default function RootLayout({
           )}
         >
           <SpeedInsights />
+          <Analytics />
 
           <NextTopLoader height={3} color="#3b82f6" />
           {/* bg animations */}
