57281eae3722e880f943a092ce65dabd0ed16a85
Rishabh Jain <rishabhjainfinal@gmail.com>
2024-03-04 11:31:15 +0530

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,23 @@
+import { getAllBlogMeta } from '@/utils/mdx';
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getAllBlogMeta();
+  const blogposts = posts.map((post) => ({
+    url: `${process.env.FRONTEND}/blog/${post.slug}`,
+    lastModified: new Date(post.date) as Date,
+    changeFrequency: 'monthly',
+    priority: 1,
+  }));
   return [
     {
       url: `${process.env.FRONTEND}/`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.FRONTEND}/blog`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
@@ -20,5 +34,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    ...blogposts as any
   ]
 }
