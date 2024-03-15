a363bff691be9f504f53fe877ee351da3ee6340a
Rishabh Jain <rishabhjainfinal@gmail.com>
2024-03-16 03:56:42 +0530

    added producthunt tag

--- src/components/pageSpecific/home/Section2.tsx
+++ src/components/pageSpecific/home/Section2.tsx
@@ -11,6 +11,18 @@ function Section2({ editorLink }: { editorLink: string }) {
           className="inline-block"
         />
       </div>
+      <a
+        href="https://www.producthunt.com/posts/build-your-resume?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-build&#0045;your&#0045;resume"
+        target="_blank"
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=445023&theme=neutral"
+          alt="Build&#0032;Your&#0032;Resume - Use&#0032;latex&#0032;to&#0032;genetate&#0032;resume&#0032;in&#0032;seconds&#0032;online | Product Hunt"
+          style={{ width: "250px", height: "54px" }}
+          className="shadow-lg shadow-[#919191d9] rounded-lg"
+        />
+      </a>
+      {/* src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=445023&theme=" */}
       <div className="flex items-center justify-center px-8 py-3 shadow lg:gap-24 bg-gradient-to-r from-blue-600 to-fuchsia-500 rounded-xl">
         <div className=" text-neutral-200 md:text-[35px] font-semibold">
           Create your Professional Package <br />
